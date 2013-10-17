696a12fb5c69419b0fae6c72807fba4d8a92abf5
Linedwell <Linedwell@hotmail.co.jp>
2013-10-17 18:59:36 +0200

    initial commit

---
+++ disambigWarningUpdater.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Script de mise à jour semi-automatique de {{Avertissements d'homonymie restants}}
+# Auteur: Linedwell
+# Licence: <à définir>
+
+import sys
+sys.path.insert(1, '..') #ajoute au PYTHONPATH le répertoire parent
+
+import re
+import time
+import locale
+locale.setlocale(locale.LC_TIME,'fr_fr') #remplacer 'fr_fr' par 'fra_fra' sur Windows
+from datetime import datetime
+
+import pywikibot
+from pywikibot import pagegenerators
+
+# Déclarations
+site = pywikibot.getSite('fr','wikipedia')
+
+def disambigWarningUpdater(pageTemp,newNumber):
+    pageTemp = re.split('(\|-\n)', pageTemp, maxsplit=3)
+    pageTempBegin = u''.join(pageTemp[:-1])
+    pageTempBody = u''.join(pageTemp[-1:])
+    pageTempBody = re.split('\|\}\n',pageTempBody)
+    pageTempEnd = u'|}\n' + u''.join(pageTempBody[-1:])
+    pageTempBody = u''.join(pageTempBody[:-1])
+    lastRaw = pageTempBody.rsplit('|-\n',1)[1]
+
+    rawObj = rawParser(lastRaw)
+    newRaw = rawMaker(rawObj,newNumber)
+
+    pageTemp = pageTempBegin + pageTempBody + newRaw + pageTempEnd
+
+    return pageTemp
+
+
+def rawParser(raw):
+    rawItems = raw.split('|')
+    
+    id = ''.join(rawItems[1].split())
+    date = ''.join(rawItems[2].split())
+    number = ''.join(re.findall('\d+',rawItems[5]))
+    
+    rawObj = {
+        'id' : id,
+        'date' : date,
+        'number' : number
+    }
+
+    return rawObj
+
+
+
+def rawMaker(prevRaw, newNumber):
+    
+    oldId = int(prevRaw['id'])
+    oldDate = prevRaw['date']
+    oldNumber = int(prevRaw['number'])
+    
+    
+    dateObj = datetime.today()
+    date = dateObj.strftime('%d %B %Y')
+    
+    oldDateObj = datetime.strptime(oldDate,'%d%B%Y')
+    
+    durationObj = dateObj - oldDateObj
+    duration = str(durationObj.days) + ' jours'
+    
+
+    diffSize = newNumber - oldNumber
+    diffPercent = (diffSize * 100.) / newNumber
+    diffPercent = round(diffPercent,2)
+    
+    if diffSize > 0:
+        diffColor = '{{rouge|+'
+    else:
+        diffColor = '{{vert|'
+
+    
+    newRaw = '|-\n| ' + str(oldId+1) + '\n| ' + date + '\n|| ' + duration + '\n| {{formatnum:' + str(newNumber) +'}} || ' + diffColor + str(diffPercent) + '%}}\n|| ' + diffColor + str(diffSize) + '}}\n'
+    
+    return newRaw
+
+
+
+
+#Exécution
+def main(args):
+    if len(args) == 1:
+        newNumber = int(args[0])
+        target = u"Modèle:Avertissements d'homonymie restants"
+        page = pywikibot.Page(site,target)
+        pageTemp = page.get()
+        pageTemp = disambigWarningUpdater(pageTemp,newNumber)
+        
+        if pageTemp != page.get():
+            page.put(pageTemp,"[[WP:Bot|Robot]] : " + u'mise à jour')
+        else:
+            print "Aucune mise à jour n'a été faite."
+    else:
+        print "usage: python disambigWarningUpdater.py <number of pages>"
+        sys.exit(2)
+
+
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv[1:])
+    finally:
+        pywikibot.stopme()
