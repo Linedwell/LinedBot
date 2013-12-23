0474fdfd1b72e817ba282a256b55de6764d2ae58
Linedwell <Linedwell@hotmail.co.jp>
2013-12-23 08:16:14 +0100

    script demandé par Guillaume70, liste sur une sous page les mouvements récents dans la Catégorie:Admissibilité à vérifier

---
+++ admissibilite.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Script indiquant les changements récents (ajouts / retraits) dans la Catégorie:Admissibilité à vérifier
+# Auteur: Linedwell
+# Licence: <à définir>
+
+import sys
+sys.path.insert(1, '..') #ajoute au PYTHONPATH le répertoire parent
+
+import pywikibot
+from pywikibot import pagegenerators
+import time
+
+# Déclarations
+site = pywikibot.getSite('fr','wikipedia')
+nbrModif = 0
+nbrTotal = 0
+
+def getCategoryContent(catname):
+    cat = pywikibot.Category(site,catname)
+    pagesInCat = list(cat.articles(False))
+    pagesList = pagegenerators.PreloadingGenerator(pagesInCat) # On génère la liste des pages incluses dans la catégorie
+    pagesList = pagegenerators.NamespaceFilterPageGenerator(pagesList,[0]) #On ne garde que les articles (Namespace 0)
+    return pagesList
+
+def titleList(pagesList):
+    list = []
+    for page in pagesList:
+        list.append(page.title())
+    
+    return list
+
+
+def saveBackupFile(pagesList):
+    file = open("_admissibilite.bak","w+")
+    for page in pagesList:
+        file.write(page.title().encode('utf-8')+'\n')
+    file.close()
+
+def loadBackupFile():
+    file = open("_admissibilite.bak","r")
+    oldList = file.readlines()
+    oldList = [s.strip('\n') for s in oldList]
+    oldList = [s.decode('utf-8') for s in oldList]
+    file.close()
+    
+    print oldList
+    return oldList
+
+
+#Exécution
+def main():
+    timeStart = time.time()
+    catname = u"Tous les articles dont l'admissibilité est à vérifier"
+    #pagesList = getCategoryContent(catname)
+    #list = titleList(pagesList)
+    #print list
+    #saveBackupFile(pagesList)
+    #print "saved"
+    loadBackupFile()
+    ###
+    timeEnd = time.time()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
