1e5cfe2657a7739efd8ad117033982fb43380e8f
Linedwell <Linedwell@hotmail.co.jp>
2014-04-06 19:09:27 +0200

    script de nettoyage du bac à sable de Vikidia

---
+++ bas.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Script de nettoyage du bac à sable de Vikidia
+# Auteur: Linedwell
+# Licence: <à définir>
+
+import sys
+sys.path.insert(1, '..') #ajoute au PYTHONPATH le répertoire parent
+
+import pywikibot
+import time
+from datetime import date, datetime, timedelta
+
+# Déclarations
+site = pywikibot.getSite('fr','vikidia')
+page = pywikibot.Page(site,u"Vikidia:Bac à sable")
+template = u"{{subst:Bac à sable}}"
+summary = u"[[Vikidia:Robot|Robot]] : Nettoyage du bac à sable"
+delay = 30
+
+
+#Recharge le bac à sable avec un contenu prédéfini
+def clean(page,template,delay):
+    if not page.userName() == site.user():
+        limite = calcLimit(delay)
+        if page.editTime() < limite:
+            page.put(template,summary)
+
+#Calcule la "date" avant laquelle on s'autorise à blanchir le bas
+def calcLimit(delay):
+    today = datetime.utcnow()
+    limit = today - timedelta(minutes=delay)
+    return limit
+
+
+#Exécution
+def main():
+    clean(page,template,delay)
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
