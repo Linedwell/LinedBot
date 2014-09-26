9de6c9243aef5fd9c7edd23d02e8697e3243a41d
Linedwell <Linedwell@hotmail.co.jp>
2014-09-26 12:04:43 +0200

    plus qu'à s'occuper du renvoi de la liste

--- inactive.py
+++ inactive.py
@@ -12,8 +12,10 @@
 import sys
 sys.path.insert(1, '..') #ajoute au PYTHONPATH le répertoire parent
 
-import pywikibot
 import time
+from datetime import date, datetime, timedelta
+
+import pywikibot
 
 # Déclarations
 site = pywikibot.getSite('fr','vikidia')
@@ -21,24 +23,44 @@ nbrModif = 0
 nbrTotal = 0
 
 
-def getInactiveSysops(duration):
+def getInactiveSysops(limit):
     
     inactiveSysops = []
     
     sysopList = site.allusers(group="sysop")
 
     for sysop in sysopList:
-        uc = site.usercontribs(user=sysop[u'name'])
+        sysopName = sysop[u'name']
+        uc = site.usercontribs(user=sysopName)
 
-    for c in uc:
-        print c[u'timestamp']
-        break
+        for c in uc:
+            lastEdit = datetime.strptime(c[u'timestamp'],"%Y-%m-%dT%H:%M:%SZ")
+            if lastEdit < limit:
+                inactiveSysops.append(u"{{u|"+ sysopName + "}} : inactif depuis le " + str(lastEdit))
+            break
+
+    print inactiveSysops
+
+
+
+#Retourne la date avant laquelle on considère obsolète l'usage du modèle
+def calcLimit(delay):
+    today = datetime.utcnow()
+    limite = today - timedelta(days=delay)
+    return limite
+
+#Retourne le temps écoulé entre une date et le jour courant
+def calcDuration(date):
+    today = datetime.utcnow()
+    duration = today - date
+    return duration
 
 
 #Exécution
 def main():
     timeStart = time.time()
-    getInactiveSysops(0)
+    limit = calcLimit(1)
+    getInactiveSysops(limit)
     timeEnd = time.time()
 
 if __name__ == "__main__":
