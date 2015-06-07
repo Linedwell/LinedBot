5da5f65144cedced777f760a355fe195b50fc8e3
Linedwell <Linedwell@hotmail.co.jp>
2015-06-07 09:01:24 +0200

    Scripts de statuts demandés par Orikrin1998

---
+++ adr.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Script de mise à jour de statut de relecteur
+
+# Auteur: Orikrin
+
+# Adapté depuis un script de Linedwell
+# Distribué sous licence GNU GPLv3
+# Distributed under the terms of the GNU GPLv3 license
+# http://www.gnu.org/licenses/gpl.html
+
+import sys
+sys.path.insert(1, '..') #ajoute au PYTHONPATH le répertoire parent
+
+import pywikibot
+import time
+
+# Déclarations
+site = pywikibot.Site('fr','wikipedia')
+statuts = ['libre','occupe','pause']
+title = u'Utilisateur:Orikrin1998/AdR'
+
+
+#Exécution
+def main(args):
+    if len(args) > 1: #argv[0] = nom du script
+        stat = args[1]
+        if stat in statuts:
+            if stat == "occupe":
+                stat = u"occupé"
+            page = pywikibot.Page(site,title)
+            pageTemp = stat
+            summary = u"Mise à jour : " + stat
+        
+            if page.get() == pageTemp:
+                print u"Aucune mise à jour nécessaire."
+            else:
+                page.text = pageTemp
+                page.save(summary)
+                print u"Action effectuée."
+        else:
+            print u"Statut '%s' inexistant\nStatuts disponibles : %s" %(stat,str(statuts))
+    else:
+        print "usage: python " + args[0] + " <statut>"
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv)
+    finally:
+        pywikibot.stopme()
---
+++ statut.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Script de mise à jour de statut
+
+# Auteur: Orikrin
+
+# Adapté depuis un script de Linedwell
+# Distribué sous licence GNU GPLv3
+# Distributed under the terms of the GNU GPLv3 license
+# http://www.gnu.org/licenses/gpl.html
+
+import sys
+sys.path.insert(1, '..') #ajoute au PYTHONPATH le répertoire parent
+
+import pywikibot
+import time
+
+# Déclarations
+site = pywikibot.Site('fr','wikipedia')
+statuts = ['online','offline','away','busy','patrol','redact','wikislow','invisible']
+title = u'Utilisateur:Orikrin1998/Statut'
+
+
+#Exécution
+def main(args):
+    if len(args) > 1: #argv[0] = nom du script
+        stat = args[1]
+        if stat in statuts:
+            page = pywikibot.Page(site,title)
+            pageTemp = u"{{statut|" + stat + "|" + title + "}}"
+            summary = u"Mise à jour : " + stat
+        
+            if page.get() == pageTemp:
+                print u"Aucune mise à jour nécessaire."
+            else:
+                page.text = pageTemp
+                page.save(summary)
+                print u"Action effectuée."
+        else:
+            print u"Statut '%s' inexistant\nStatuts disponibles : %s" %(stat,str(statuts))
+    else:
+        print "usage: python " + args[0] + " <statut>"
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv)
+    finally:
+        pywikibot.stopme()
